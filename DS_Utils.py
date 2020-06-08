361cb2795cf37d47447683e547418633d955c49b
philippkaindl <philpp.kaindl@hotmail.com>
2020-06-08 14:02:40 +0200

    Inital Commit

---
+++ DS_Utils.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def display_all(df):
+    with pd.option_context("display.max_rows", 1000): 
+        with pd.option_context("display.max_columns", 1000): 
+            display(df)
