d0168e2e1b02662883c2f01435b82cd6d73b0a15
philippkaindl <philpp.kaindl@hotmail.com>
2020-06-09 19:07:46 +0200

    Not dropping NA Columns

--- DS_Utils.py
+++ DS_Utils.py
@@ -11,3 +11,16 @@ def plotMissingValuesDF(df, percentage = True):
         missingValuesofDF = missingValuesofDF/df.shape[0]
 
     missingValuesofDF.plot.bar()
+
+
+
+def dropColumnsAbovePercentage(df, percentage = 0.8):
+    ''' Takes a dataframe and checks how many missing values there are in it. Then drops all the columns that do not have it. '''
+    NrMissing = df.isnull().sum()
+    dropColms = NrMissing[NrMissing/df.shape[0] > percentage].index
+    if len(dropColms) > 0: 
+        print(f'Cut off percentage is set to {percentage}. Dropping columns: {dropColms}')
+        return df.drop(columns = dropColms, axis = 1)
+    else: 
+        print(f'Cut off percentage is set to {percentage}. Dropping no columns as condition is not met')
+        return None 
